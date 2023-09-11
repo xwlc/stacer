0bc6134478d4964b255a6606172c56c2b6528e7d
QuentiumYT <quentin.lienhardt@gmail.com>
2023-09-11 10:18:20 +0200

    Bump version to 1.3.0

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.2.2
+VERSION=1.3.0
 DIR=stacer-$VERSION
 export VERSION=$VERSION
 
--- stacer/main.cpp
+++ stacer/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 
     qApp->setApplicationName("stacer");
     qApp->setApplicationDisplayName("Stacer");
-    qApp->setApplicationVersion("1.2.2");
+    qApp->setApplicationVersion("1.3.0");
     qApp->setWindowIcon(QIcon(":/static/logo.png"));
 
     {
